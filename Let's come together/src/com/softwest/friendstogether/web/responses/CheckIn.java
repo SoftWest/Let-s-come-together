96d561290717adfb7d417242c16bd9ce7f4c0924
TarasMatolinets <ernest.hyst@gmail.com>
2014-04-15 20:43:21 +0300

    solve conflicts

---
+++ Let's come together/src/com/softwest/friendstogether/web/responses/CheckIn.java
@@ -0,0 +1,9 @@
+package com.softwest.friendstogether.web.responses;
+
+import com.softwest.friendstogether.web.responses.wrappers.ResponseBody;
+
+public class CheckIn
+  extends ResponseBody
+{
+  
+}
